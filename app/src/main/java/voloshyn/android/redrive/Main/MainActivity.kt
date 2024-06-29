244af39593ba9e20e30cb1db35ccb9428c416544
Ivan Voloshyn <ivan.voloshyn.fs@gmail.com>
2024-06-29 16:07:09 +0200

    Create onBoards layouts

---
+++ app/src/main/java/voloshyn/android/redrive/Main/MainActivity.kt
@@ -0,0 +1,6 @@
+package voloshyn.android.redrive.Main
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+}
---
+++ app/src/main/java/voloshyn/android/redrive/onBoard/OnBoardActivity.kt
@@ -0,0 +1,7 @@
+package voloshyn.android.redrive.onBoard
+
+import androidx.appcompat.app.AppCompatActivity
+import voloshyn.android.redrive.R
+
+class OnBoardActivity :AppCompatActivity(R.layout.activity_on_board){
+}
