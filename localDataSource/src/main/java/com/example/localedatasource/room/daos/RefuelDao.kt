a01469cd385c2a2439a11941941b6e2bef5e1143
Ivan Voloshyn <ivan.voloshyn.fs@gmail.com>
2025-06-01 09:33:28 +0200

    Code refactor

--- localDataSource/src/main/java/com/example/localedatasource/room/daos/RefuelDao.kt
+++ localDataSource/src/main/java/com/example/localedatasource/room/daos/RefuelDao.kt
@@ -52,7 +52,4 @@ interface RefuelDao {
     @Query("SELECT SUM(fuel_volume*unit_price) FROM refuels WHERE vehicle_id=:currentVehicleId")
     fun observePaymentSum(currentVehicleId: String): Flow<Double?>
 
-    fun hasPending(currentUserId:String):Flow<Boolean>
-
-
 }
